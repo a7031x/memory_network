34853760dbe1d5c9e0d7bc7947fd47fdd11cd79d
easton <easton.hou@gmail.com>
2018-07-20 16:52:28 +0800

    Merge b.ranch 'using_rnn' into gru_uk
    .

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ import numpy as np
 import func
 import utils
 import os
+import rnn
 
 
 class Model(nn.Module):
@@ -18,7 +19,31 @@ class Model(nn.Module):
         self.reset_parameters(self.A)
         self.C = nn.ModuleList()
         self.encoding = self.position_encoding(max_sentence_size, embedding_size)
-        self.gru = nn.GRUCell(embedding_size, embedding_size)
+        num_layers = 2
+        self.qrnn = rnn.RNNEncoder(
+            input_size=embedding_size,
+            hidden_size=embedding_size,
+            num_layers=num_layers,
+            bidirectional=True,
+            batch_first=True,
+            type='gru')
+        self.srnn = rnn.RNNEncoder(
+            input_size=embedding_size,
+            hidden_size=embedding_size,
+            num_layers=num_layers,
+            bidirectional=True,
+            batch_first=True,
+            type='gru')
+        self.crnn = rnn.RNNEncoder(
+            input_size=embedding_size,
+            hidden_size=embedding_size,
+            num_layers=num_layers,
+            bidirectional=True,
+            batch_first=True,
+            type='gru')
+        self.gru = nn.GRUCell(embedding_size*num_layers, embedding_size*num_layers)
+        self.dense_state = nn.Linear(embedding_size*num_layers, embedding_size, bias=False)
+        self.reset_parameters(self.dense_state)
         for _ in range(hops):
             C = nn.Embedding(vocab_size, embedding_size, padding_idx=data.PAD_ID)
             self.reset_parameters(C)
@@ -31,6 +56,17 @@ class Model(nn.Module):
 
     def reset_parameters(self, x):
         x.weight.data = x.weight.data / 2
+
+
+    def run_state(self, rnn, emb, state):
+        if emb.dim() == 4:
+            n, m, l, d = emb.shape
+            state = self.run_state(rnn, emb.view(n*m, l, d), state.view(rnn.num_states, n*m, -1) if state is not None else None)
+            return state.view(rnn.num_states, n, m, -1)
+        lengths = torch.ones(emb.shape[0], emb.shape[1])
+        _, state = rnn(emb, lengths, state)
+        n = emb.shape[0]
+        return state
 
 
     def position_encoding(self, sentence_size, embedding_size):
@@ -49,30 +85,40 @@ class Model(nn.Module):
         return func.tensor(np.transpose(encoding))
 
 
+    def restack(self, state):
+        states = torch.unbind(state, 0)
+        return torch.cat(states, -1)
+
+
+    def stack(self, state):
+        states = state.chunk(self.crnn.num_states, dim=-1)
+        return torch.stack(states, 0)
+
+
     def forward(self, stories, queries):
         q_emb = self.A(queries)#[batch,slen,dim]
         #self.encoding = 1
-        u_k = (q_emb * self.encoding).sum(1)#[batch, dim]
+        u_k = self.run_state(self.qrnn, q_emb * self.encoding, None)#[num_states, batch, dim]
 
         for hop in range(self.hops):
+            emb_A = self.A(stories)#[batch,mlen,slen,dim]
             if hop == 0:
-                emb_A = self.A(stories)#[batch,mlen,slen,dim]
+                A = self.run_state(self.srnn, emb_A * self.encoding, None)#[num_states, batch, mlen, dim]
             else:
-                emb_A = self.C[hop-1](stories)
-            A = (emb_A * self.encoding).sum(2)#[batch, mlen, dim]
-            dotted = torch.einsum('bmd,bd->bm', (A, u_k))#[batch,mlen]
+                A = self.run_state(self.srnn, emb_A * self.encoding, A)
+            dotted = torch.einsum('bmd,bd->bm', [self.restack(A), self.restack(u_k)])#[batch,mlen]
             #mask = stories.sum(-1) != 0
             #dotted -= (1-mask.float()) * 10000
             probs = nn.functional.softmax(dotted, -1).clone()#[batch,mlen]
 
-            emb_C = self.C[hop](stories)#[batch,mlen,slen,dim]
-            C = (emb_C * self.encoding).sum(2)#[batch,mlen,dim]
-
-            o_k = torch.einsum('bmd,bm->bd', (C, probs))
+            #emb_C = self.C[hop](stories)#[batch,mlen,slen,dim]
+            #C = (emb_C * self.encoding).sum(2)#[batch,mlen,dim]
+            C = self.run_state(self.crnn, emb_A * self.encoding, None)#[num_states, batch, mlen, dim]
+            o_k = torch.einsum('bmd,bm->bd', (self.restack(C), probs))
             #u_k = u_k + o_k
-            u_k = self.gru(o_k, u_k)
-        
-        return torch.matmul(u_k, self.C[-1].weight.transpose(0, 1))
+            u_k = self.stack(self.gru(o_k, self.restack(u_k)))
+        u = self.dense_state(self.restack(u_k))
+        return torch.matmul(u, self.A.weight.transpose(0, 1))
 
 
     def load(self, filename):
