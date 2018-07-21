cd129823e0f0f0551ab6d16cacd24fd619cd84a1
easton <a7031x@gmail.com>
2018-07-21 03:21:42 +0000

    .

--- models.py
+++ models.py
@@ -111,8 +111,8 @@ def make_loss_compute():
 def build_model(opt, dataset=None):
     dataset = dataset or data.Dataset(opt)
     model = Model(dataset.vocab_size, opt.embedding_size, dataset.sentence_size, opt.hops)
-    #external_checkpoint = './init.pkl'
-    #model.load(external_checkpoint)
+    external_checkpoint = './init.pkl'
+    model.load(external_checkpoint)
     if func.gpu_available():
         model = model.cuda()
     return model
